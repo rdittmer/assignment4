e944692dd6b531d02a57ed4f2aa620d1d68f960e
Ryan Dittmer <rdittmer@fullsail.edu>
2012-01-26 21:40:05 -0500

    added replaceSeparator function

--- Dittmer_Ryan_assignment4.js
+++ Dittmer_Ryan_assignment4.js
@@ -1,1 +1,22 @@
-alert("JavaScript works!");
+/*Ryan Dittmer  
+  01/19/2012    
+  Deliverable 4 
+  Library  */
+  
+
+  //Replaces the first separator in the given string with the second separator
+  function replaceSeparator( string, firstSep, secondSep )
+  {
+    for ( var i = 0; i < string.length; ++i )
+  	{
+  		if ( string.charAt( i ) == firstSep )
+  		{
+  			string = string.replace( firstSep, secondSep );
+  		}
+  	}
+  	console.log(string);
+  	return string;
+  }
+  
+  
+  
